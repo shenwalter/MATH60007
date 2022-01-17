178b11b74cd5537edc8f10a1c97ce4946242409f
Walter Shen <shen.walter@yahoo.com>
2022-01-17 12:49:02 +0000

    Updated files with corrections
    Corrections to custom Axelrod strategy code and affected results in Jupyter notebook

---
+++ wws_v2.py
@@ -0,0 +1,32 @@
+class WWS(Player):
+    """
+    Half Lazy TFT
+
+    A player starts by cooperating and then on only responds to opponent defects
+    every other time, otherwise the player just cooperates.
+
+    Code is slight moficiation of original TFT strategy.
+    Can be classified as "nice".
+
+    """
+
+    # These are various properties for the strategy
+    name = "Half Lazy TFT"
+    classifier = {
+        "memory_depth": 1,  # Four-Vector = (1.,0.,1.,0.)
+        "stochastic": False,
+        "long_run_time": False,
+        "inspects_source": False,
+        "manipulates_source": False,
+        "manipulates_state": False,
+    }
+
+    def strategy(self, opponent: Player) -> Action:
+        """This is the actual strategy"""
+        # First move
+        if not self.history:
+            return C
+        # React to the opponent's every other move (if defection)
+        if opponent.history[-1] == D and opponent.defections>1 and opponent.defections%2==0:
+            return D
+        return C
